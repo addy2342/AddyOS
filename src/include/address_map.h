707db6c1de5c3555747278d62321cc186d825791
addy2342 <azden@web.de>
2015-02-21 23:08:03 -0400

    added LED address

--- src/include/address_map.h
+++ src/include/address_map.h
@@ -1,8 +1,10 @@
 #ifndef __ADDRESS_MAP__
 #define __ADDRESS_MAP__
 
-#define HEX7_HEX4_BASE 0x10000030
+#define LEDR 0x10000000
+#define LEDG 0x10000010
 #define HEX3_HEX0_BASE 0x10000020
+#define HEX7_HEX4_BASE 0x10000030
 #define LCD 0x10003050
 
 #endif /* _ADDRESS_MAP_H_  */
